da717eee0de0620bcda29262a7b4e4c3f5d28b99
leonardoolia <leonardooliaa@gmail.com>
2024-01-16 16:50:11 +0100

    snack1 - created array of objects

--- snack1/script.js
+++ snack1/script.js
@@ -1,1 +1,42 @@
-console.log('js ok')
+//? Nome del tavolo  e la lista degli invitati in ordine di posto.
+const tableName = 'Tavolo Vip';
+
+const guests = [
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
+];
+
+/* Risultato da ottenere
+const guestList = [
+    {
+        tableName = '',
+        guestName = '',
+        place = 1
+    }
+]
+*/
+
+//? Svolgimento esercizio
+
+const finalGuestList = [];
+
+guests.forEach((guest, i) => {
+
+    const guestList = {
+        tableName,
+        guestName: guest,
+        place: ++i
+    }
+
+    finalGuestList.push(guestList);
+});
+
+console.log(finalGuestList)
